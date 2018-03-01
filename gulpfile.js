180db411ac02edcde438b4021b4192d86910d99f
Sagar <sgavhane.dev@gmail.com>
2018-03-01 23:31:26 +0530

    task runner gulp add

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+const gulp = require('gulp'),
+      merge = require('gulp-merge-json');
+
+gulp.task('json', function(){
+    gulp.src('src/*.json')
+        .pipe(merge({
+            fileName: 'snippet.json',
+
+        }))
+        .pipe(gulp.dest('./dist/'));
+});
